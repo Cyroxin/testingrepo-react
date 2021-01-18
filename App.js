d853ec165111be964a8c7128a8dd980070973c46
Cyroxin <34924561+Cyroxin@users.noreply.github.com>
2021-01-18 19:11:27 +0200

    Create Navigation

--- App.js
+++ App.js
@@ -1,38 +1,11 @@
 /* eslint-disable object-curly-spacing */
 /* eslint-disable max-len */
-import {
-  StatusBar,
-} from 'expo-status-bar';
-import List from './components/List';
-import React, { useEffect, useState } from 'react';
-import { Platform, SafeAreaView } from 'react-native';
+import React from 'react';
+import Navigator from './navigators/Navigator';
 
-const url =
-  'https://raw.githubusercontent.com/Cyroxin/testingrepo-react/week1-http-a/data/cats.json';
 
 const App = () => {
-  const [mediaArray, setmediaArray] = useState([]);
-
-  const loadMedia = async () => {
-    try {
-      const response = await fetch(url);
-      const json = await response.json();
-      setmediaArray(json);
-    } catch (exp) {
-      console.log(exp.message);
-    }
-  };
-
-  useEffect(() => {
-    loadMedia();
-  }, []);
-
-  return (
-    <SafeAreaView style={{ paddingTop: Platform.OS === 'android' ? 25 : 0 }}>
-      <StatusBar style='auto' />
-      <List mediaArray= {mediaArray} />
-    </SafeAreaView>
-  );
+  return <Navigator></Navigator>;
 };
 
 export default App;
---
+++ views/Home.js
@@ -0,0 +1,35 @@
+import {StatusBar} from 'expo-status-bar';
+import List from '../components/List';
+import React, {useEffect, useState} from 'react';
+import {Platform, SafeAreaView} from 'react-native';
+
+const url =
+  'https://raw.githubusercontent.com/Cyroxin/testingrepo-react/week1-http-a/data/cats.json';
+
+
+const Home = () => {
+  const [mediaArray, setmediaArray] = useState([]);
+
+  const loadMedia = async () => {
+    try {
+      const response = await fetch(url);
+      const json = await response.json();
+      setmediaArray(json);
+    } catch (exp) {
+      console.log(exp.message);
+    }
+  };
+
+  useEffect(() => {
+    loadMedia();
+  }, []);
+
+  return (
+    <SafeAreaView style={{paddingTop: Platform.OS === 'android' ? 25 : 0}}>
+      <StatusBar style='auto' />
+      <List mediaArray={mediaArray} />
+    </SafeAreaView>
+  );
+};
+
+export default Home;
